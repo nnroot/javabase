cd3d0f190adc758ad193051e298dc85876a0d751
nnroot <77209659+nnroot@users.noreply.github.com>
2021-04-08 19:32:43 +0100

    boucle for
    main

---
+++ BoucleFor.java
@@ -0,0 +1,15 @@
+    public static void main(String[] args) {
+
+        for (int nombre = 2;nombre<100;nombre++){
+            boolean test = true;
+
+            for (int start = 5;start<nombre;start++){
+                if (nombre%start==0){
+                    test = false;
+                    break;
+                }
+            }
+            if (test) {
+                System.out.println(nombre);
+            }
+        }
