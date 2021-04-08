be41a0ecbbb68bb7c5874b27abdd239b926b4f9f
nnroot <77209659+nnroot@users.noreply.github.com>
2021-04-08 19:47:02 +0100

    hello world
    ok ok ok

--- main.java
+++ main.java
@@ -1,16 +1,5 @@
-public class main {
-    public static void main(String[] args) 
-
-        for (int nombre = 2;nombre<100;nombre++){
-            boolean test = true;
-
-            for (int start = 5;start<nombre;start++){
-                if (nombre%start==0){
-                    test = false;
-                    break;
-                }
-            }
-            if (test) {
-                System.out.println(nombre);
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
             }
         }
